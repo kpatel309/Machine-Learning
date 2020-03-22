191aed2046c937af356377fe5129bae73a696d75
Kushal Patel <kpatel309@gmail.com>
2020-03-22 09:44:47 -0400

    Adding files.

--- dev/Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
+++ dev/Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
@@ -11,12 +11,11 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
-# Many libraries handle feature scaling for us
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
